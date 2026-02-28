32b79ac5c39f3d30cb9d383edb3fd36cb637dc6c
yhy <olablrmiyinebanniye@gmail.com>
2026-02-28 15:09:38 +0300

    Update checks.py

--- checks.py
+++ checks.py
@@ -1,21 +1,29 @@
 from datetime import datetime
 import os
 import json
+import logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - [%(levelname)s] - %(message)s',
+    datefmt='%H:%M:%S'
+)
+
+
 class startup_check:
     def app_startup(self, connected_devicesips, current_lang, data):
         self.connected_devicesips = connected_devicesips
-        print(f"{data[current_lang]['l1']}")
+        logging.debug("%s", data[current_lang]['l1'])
         default_path = os.path.dirname(os.path.abspath(__file__))
         file_path = os.path.join(default_path, "check.json")
 
         now = datetime.now()
-        #if not os.path.exists(file_path):
+        # if not os.path.exists(file_path):
         json_default_data = {
             "last_entered": f"{now}",
             "last_commands": {},
             "connected_ips": {}
         }
-        #else:
+        # else:
         #    pass
 
         if not os.path.exists(file_path):
@@ -26,12 +34,8 @@ class startup_check:
         else:
             with open(file_path, "r", encoding="utf-8") as f:
                 check_data = json.load(f)
-                #json.dump(json_default_data, f, indent=4, ensure_ascii=False)
-                #pass
+                # json.dump(json_default_data, f, indent=4, ensure_ascii=False)
+                # pass
         json_ip = check_data["connected_ips"]
         connected_devicesips.configure(text="\n".join(json_ip.keys()))
-
-        #CONTROLLING SOMETHINGS
-        with open(file_path, "r", encoding="utf-8") as file:
-            content = file.read()
-        print(f"{data[current_lang]["l2"]}")
+        logging.debug("%s", data[current_lang]["l2"])
